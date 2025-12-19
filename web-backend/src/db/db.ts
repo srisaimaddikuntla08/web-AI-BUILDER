19482992762d3244f89b367d6c37d2d85b4af585
srisai810 <srisaimaddikuntlams@gmail.com>
2025-12-19 06:26:12 +0530

    cv

--- web-backend/src/db/db.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config'
-import { PrismaClient } from '../../prisma/generated/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-
-const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
-const prisma = new PrismaClient({ adapter: pool })
-
-export default prisma;
