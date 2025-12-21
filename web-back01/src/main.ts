cef0b674d3484cd32f0e658e0b86ef0cce8719d9
srisai810 <srisaimaddikuntlams@gmail.com>
2025-12-21 10:41:44 +0530

    first commit

--- web-back01/src/main.ts
+++ web-back01/src/main.ts
@@ -2,13 +2,11 @@ import express from 'express'
 const app = express();
 import 'dotenv/config'
 import cors from 'cors'
-
-import { toNodeHandler } from "better-auth/node";
+import { toNodeHandler } from 'better-auth/node';
 import { auth } from './lib/auth';
-
+import userRoute from './routes/userRoutes';
 
 const port = 3000;
-
 
 const corsOptions = {
     origin: process.env.TRUSTED_ORIGIN?.split(',') || [] ,
@@ -16,16 +14,12 @@ const corsOptions = {
 }
 
 app.use(cors(corsOptions))
+app.use(express.json({limit:"50mb"}))
+app.use('/api/user',userRoute)
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
 
-
-
-app.get('/',(req,res)=>{
-    return res.json({message :"working of server"})
-})
-
-
+app.get("/",(req,res)=> res.send("hello"))
 
 
 
